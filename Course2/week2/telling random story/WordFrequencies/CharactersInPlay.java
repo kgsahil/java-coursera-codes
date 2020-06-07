bef98fb604a9f4a4db3516a48437c4493b3cf450
Sahil Gupta <sahilgupta.17299@gmail.com>
2020-06-08 00:07:38 +0530

    done with week2 1st part of the week2

---
+++ Course2/week2/telling random story/WordFrequencies/CharactersInPlay.java
@@ -0,0 +1,50 @@
+import edu.duke.*;
+import java.util.*;
+
+public class CharactersInPlay {
+    ArrayList<String> characters;
+    ArrayList<Integer> counts;
+    
+    public CharactersInPlay(){
+        characters = new ArrayList<String>();
+        counts = new ArrayList<Integer>();
+    }
+    private void update(String person){
+        if(characters.contains(person)){
+            int index = characters.indexOf(person);
+            int value = counts.get(index)+1;
+            counts.set(index,value);
+        }else{
+            characters.add(person);
+            counts.add(1);
+        }
+    }
+    private void findAllCharacters(){
+        characters.clear();
+        counts.clear();
+        FileResource fr = new FileResource();
+        for(String line : fr.lines()){
+            int index = line.indexOf(".");
+            if(index != -1){
+                String name = line.substring(0,index);
+                update(name);
+            }
+        }
+    }
+    private void charactersWithNumberParts(int num1,int num2){
+        for(int i=0;  i<characters.size(); i++){
+            int count = counts.get(i);
+            if(count > num1 && count <= num2)
+            System.out.println(count+" "+characters.get(i));
+        }
+    }
+    public void tester(){
+        findAllCharacters();
+        // for(int i=0;  i<characters.size(); i++){
+            // if(counts.get(i) > 1)
+            // System.out.println(counts.get(i)+" "+characters.get(i));
+        // }
+        charactersWithNumberParts(10,15);
+        
+    }
+}
